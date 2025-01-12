2f4360ff8b39c6b129e505ad94080402a390e37b
SHASHI9705 <singhshashiranjan34@gmail.com>
2025-01-12 19:36:14 +0530

    js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+const shell = document.getElementById('headshellicon')
+
+
+document.getElementById('headshellicon').onclick = () => document.body.style.color = document.body.style.color === 'black' ? 'white' : 'black';
+document.getElementById('headshellicon').onclick = () => document.body.style.backgroundColor = document.body.style.backgroundColor === 'white' ? '#161d26' : 'white';
