529a240248a786c0b1114c19d8e37114f8c0d82c
SHASHI9705 <singhshashiranjan34@gmail.com>
2025-01-12 23:56:01 +0530

    colors

--- script.js
+++ script.js
@@ -3,5 +3,10 @@
 const shell = document.getElementById('headshellicon')
 
 
-document.getElementById('headshellicon').onclick = () => document.body.style.color = document.body.style.color === 'black' ? 'white' : 'black';
-document.getElementById('headshellicon').onclick = () => document.body.style.backgroundColor = document.body.style.backgroundColor === 'white' ? '#161d26' : 'white';
+document.getElementById('headshellicon').onclick = () => {
+    document.body.style.backgroundColor = document.body.style.backgroundColor === 'white' ? '#161d26' : 'white';
+    document.body.style.color = document.body.style.color === 'black' ? 'white' : 'black';
+    document.querySelector('.bodyfooter').style.color = document.querySelector('.bodyfooter').style.color === 'white' ? 'white' : 'white';
+
+
+} 
