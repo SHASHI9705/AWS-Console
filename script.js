a946ac939360f228bd02ebafc6cf5b206665449a
SHASHI9705 <singhshashiranjan34@gmail.com>
2025-01-16 02:42:10 +0530

    started with mode

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+const shell = document.getElementById('headshellicon');
+const root = document.documentElement;
 
 
-const shell = document.getElementById('headshellicon')
+shell.onclick = () => {
 
-
-document.getElementById('headshellicon').onclick = () => {
-    document.body.style.backgroundColor = document.body.style.backgroundColor === 'white' ? '#161d26' : 'white';
-    document.body.style.color = document.body.style.color === 'black' ? 'white' : 'black';
-    document.querySelector('.bodyfooter').style.color = document.querySelector('.bodyfooter').style.color === 'white' ? 'white' : 'white';
-
-
-} 
+    const currentBackground = getComputedStyle(document.body).backgroundColor;
+    if (currentBackground === 'rgb(255, 255, 255)') { // 'white' in RGB
+        document.body.style.backgroundColor = '#161d26';
+        document.body.style.color = 'white';
+        document.querySelector('.bodyfooter').style.color = 'white';
+        root.style.setProperty('--head-names', '#42b4ff');
+    } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+        document.querySelector('.bodyfooter').style.color = 'black';
+        root.style.setProperty('--head-names', '#006ce0');
+    }
+};
