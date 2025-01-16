7f113ce6601f40be0ca69274774733aa4f93c0a6
SHASHI9705 <singhshashiranjan34@gmail.com>
2025-01-17 00:32:24 +0530

    current region

--- script.js
+++ script.js
@@ -8,12 +8,16 @@ shell.onclick = () => {
     if (currentBackground === 'rgb(255, 255, 255)') { // 'white' in RGB
         document.body.style.backgroundColor = '#161d26';
         document.body.style.color = 'white';
+        document.querySelector('.pannel2regionselect').style.color = "white"
+        document.querySelector('.detailselect').style.color = "white"
         document.querySelector('.bodyfooter').style.color = 'white';
         root.style.setProperty('--head-names', '#42b4ff');
     } else {
         document.body.style.backgroundColor = 'white';
         document.body.style.color = 'black';
-        document.querySelector('.bodyfooter').style.color = 'black';
+        document.querySelector('.pannel2regionselect').style.color = "black"
+        document.querySelector('.detailselect').style.color = "black"
+        document.querySelector('.bodyfooter').style.color = 'white';
         root.style.setProperty('--head-names', '#006ce0');
     }
 };
