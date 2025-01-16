7caa8ce81a5ad7417e5c0ac52d0d08b00374eeb6
SHASHI9705 <singhshashiranjan34@gmail.com>
2025-01-17 01:47:04 +0530

    dark mode

--- script.js
+++ script.js
@@ -9,15 +9,25 @@ shell.onclick = () => {
         document.body.style.backgroundColor = '#161d26';
         document.body.style.color = 'white';
         document.querySelector('.pannel2regionselect').style.color = "white"
-        document.querySelector('.detailselect').style.color = "white"
+        document.querySelectorAll('.detailselect').forEach((element) =>{
+            element.style.color = "white"
+        });
         document.querySelector('.bodyfooter').style.color = 'white';
         root.style.setProperty('--head-names', '#42b4ff');
+        root.style.setProperty('--hover-color', '#7fccff');
     } else {
         document.body.style.backgroundColor = 'white';
         document.body.style.color = 'black';
         document.querySelector('.pannel2regionselect').style.color = "black"
-        document.querySelector('.detailselect').style.color = "black"
+        document.querySelectorAll('.detailselect').forEach((element) =>{
+            element.style.color = "black"
+        });
         document.querySelector('.bodyfooter').style.color = 'white';
         root.style.setProperty('--head-names', '#006ce0');
+        root.style.setProperty('--hover-color', '#00008b');
+
     }
 };
+
+
+
